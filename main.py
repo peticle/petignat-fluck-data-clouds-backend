1c331bb5ed290ef9b19ad5a770502066ac181f25
Clément Petignat <clement.petignat@he-arc.ch>
2023-05-10 21:35:08 +0200

    Add a route to the Stable Diffusion instance

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
-from fastapi import FastAPI, Request
+"""Add routes for the application."""
+import requests
+
 import uvicorn
+from fastapi import FastAPI, Request, Response
+from fastapi.exceptions import HTTPException
+from pydantic import BaseModel
 
 
 app = FastAPI()
+
+SD_ADDR = "10.0.0.5"
+SD_PORT = "7860"
+SD_URL = f"https://{SD_ADDR}:{SD_PORT}/sdapi/v1/txt2img/"
+
+
+class StablePayload(BaseModel):
+    """Represents a payload to generate an image with Stable Diffusion.
+
+    Attributes:
+        text: The prompt text used to generate an image.
+        steps: The number of steps used to generate the image.
+    """
+    prompt: str
+    steps: int
+
 
 PEOPLE = [
     {'name': 'Alice', 'age': 42, 'city': 'London'},
@@ -17,10 +38,23 @@ async def people(request: Request):
     return {'people': PEOPLE}
 
 
-@app.get('/people/{index}')
-async def person(request: Request, index: int):
-    print('Request for person page received with index=%d' % index)
-    return {'person': PEOPLE[index]}
+@app.post('/imgen')
+async def stable_diffusion(payload: StablePayload):
+    """A route to generate an image with Stable Diffusion."""
+    # Send the request to the Stable Diffusion instance
+    PAYLOAD = payload.dict()
+    response = requests.post(url=SD_URL, json=PAYLOAD)
+
+    # Check if we received a response from Stable Diffusion
+    if response.status_code != 200:
+        raise HTTPException(
+            status_code=500,
+            detail="Could not contact Stable Diffusion",
+        )
+
+    # Return the generated image in base64
+    r = response.json()
+    return Response(r["images"][0])
 
 if __name__ == '__main__':
     uvicorn.run('main:app', host='0.0.0.0', port=8000)
